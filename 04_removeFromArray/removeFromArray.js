9dde2fb4c37d8fb0494614af2e57712a596ae2a4
Joseph Leathem <jleat2620@gmail.com>
2024-10-02 15:02:37 -0700

    Completed assignment 4, This one was tricky and required research that I did not want to do

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const [array, ...itemsToRemove] = args; // Destructure the arguments into `array` (the first argument) and `itemsToRemove` (the rest of the arguments)
+    return array.filter(item => !itemsToRemove.includes(item)); // Use the `filter` method to create a new array containing only the elements from `array` that are not included in `itemsToRemove`.
+
 };
 
 // Do not edit below this line
