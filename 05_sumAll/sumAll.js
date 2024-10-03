55f99a6c65957d2cc021a04374c7ab46743b6be2
Joseph Leathem <jleat2620@gmail.com>
2024-10-03 13:57:12 -0400

    Half way done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0){
+        return ('ERROR')
+    } else {
+        let sum = 0;
 
+        if (end > start) {
+            let diff = end - start 
+            let i = 0
+            while (i <= diff) {
+                sum += i 
+                i++
+            }
+        let sumAll = sum + end;
+            return sumAll
+        } else if (start > end) {
+            let diff = start - end
+            let i = 0
+            while (i <= diff){
+                sum += i
+                i++
+            }   
+        let sumAll = sum + start;
+            return sumAll
+        }
+    }
+    
 };
 
 // Do not edit below this line
