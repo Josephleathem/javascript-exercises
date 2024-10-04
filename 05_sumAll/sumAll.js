34f72f0ec08eeb67a5bbd4419c5800793d916051
joseph <jleat2620@gmail.com>
2024-10-03 22:22:09 -0400

    assignment Complete, It took time to figure out and place non inegter and non number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,35 +1,31 @@
 const sumAll = function(start, end) {
-    if (start < 0){
+    if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)){
         return ('ERROR')
-    } else {
-        let sum = 0;
-
-        if (end > start) {
-            if (start != Number.isInteger(start)){
-                 return ('ERROR');
-            } else {
-                let diff = end - start 
-                let i = 0
-                while (i <= diff) {
-                    sum += i 
-                    i++
-                }
-            let sumAll = sum + end;
-                return sumAll
-            }
-        } else if (start > end) {
-            let diff = start - end
-            let i = 0
-            while (i <= diff){
-                sum += i
-                i++
-            }   
-        let sumAll = sum + start;
-            return sumAll
-        }
     }
 
+    let sum = 0;
+        
+    if (end > start) {
+            let diff = end - start 
+            let i = 0
+            while (i <= diff) {
+                sum += i 
+                i++
+            }
+        let sumAll = sum + end;
+            return sumAll
+    } else if (start > end) {
+        let diff = start - end
+        let i = 0
+        while (i <= diff){
+            sum += i
+            i++
+        }   
+    let sumAll = sum + start;
+        return sumAll
+    }
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
